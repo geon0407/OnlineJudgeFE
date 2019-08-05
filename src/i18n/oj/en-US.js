abd4b3868f242f7b73b5252ce3bad4bcfde89aae
geon0407 <53281908+geon0407@users.noreply.github.com>
2019-08-05 18:41:58 +0900

    asdqwe

--- src/i18n/oj/en-US.js
+++ src/i18n/oj/en-US.js
@@ -16,32 +16,27 @@ export const m = {
   Score: '점수',
   Tags: '태그',
   Show: '보기',
-  
   // About.vue
   Result_Explanation: '채점 결과 설명',
   Pending_Juding: '서버가 열심히 채점하고 있습니다. 잠시만 기다려주세요.',
-  Compile_Error: "소스코드 컴파일에 실패하였습니다. 컴파일러의 출력결과를 보려면 클릭하세요.",
+  Compile_Error: '소스코드 컴파일에 실패하였습니다. 컴파일러의 출력결과를 보려면 클릭하세요.',
   Accepted: '축하합니다! 정답입니다!',
-  Wrong_Answer: "아쉽지만 오답입니다.",
+  Wrong_Answer: '아쉽지만 오답입니다.',
   Runtime_Error: '제출한 프로그램이 비정상적으로 종료되었습니다. 세그먼트 에러 등을 확인해보세요.',
   Time_Limit_Exceeded: '실행 제한시간을 조과하였습니다. JAVA는 3배의 시간이 주어집니다.',
   Memory_Limit_Exceeded: '실행 메모리제한을 초과하였습니다.',
-  System_Error: '앗! 서버에 문제가 생겼습니다. 관리자에게 보고해주세요.',
-  
+  System_Error: '앗! 서버에 문제가 생겼습니다. 관리자에게 보고해주세요.',  
   // FAQ.vue
-  Frequently_Asked_Questions: '자주 묻는 질문들',
-  
+  Frequently_Asked_Questions: '자주 묻는 질문들',  
   // ContestDetail.vue
   Problems: '문제',
   Announcements: '공지사항',
   Submissions: '제출기록',
   Rankings: '순위',
   Overview: '개요',
-  Admin_Helper: 'Admin Helper',
-  
+  Admin_Helper: 'Admin Helper',  
   // ContestProblemList
   Problems_List: '문제 목록',
-
   // NavBar.vue
   Home: '메인',
   NavProblems: '문제',
@@ -59,39 +54,32 @@ export const m = {
   MySubmissions: '내 채점기록',
   Settings: '프로필 설정',
   Management: '관리자메뉴',
-  Logout: '로그아웃',
-  
+  Logout: '로그아웃',  
   // announcements.vue
   Refresh: '새로고침',
   Back: '뒤로가기',
-  No_Announcements: '공지가 없습니다.',
-  
+  No_Announcements: '공지가 없습니다.',  
   // Setting.vue
   Profile: '프로필',
   Account: '회원정보',
-  Security: '보안',
-  
+  Security: '보안',  
   // AccoutSetting.vue
   ChangePassword: '비밀번호 변경',
   ChangeEmail: 'Email 변경',
-  Update_Password: '비밀번호 업데이트',
-  
+  Update_Password: '비밀번호 업데이트',  
   // ProfileSetting.vue
   Avatar_Setting: '아바타 설정',
-  Profile_Setting: '프로필 설정',
-  
+  Profile_Setting: '프로필 설정',  
   // SecuritySettig
   Sessions: '세션',
-  Two_Factor_Authentication: '이중인증',
-  
+  Two_Factor_Authentication: '이중인증',  
   // Login.vue
   LoginUsername: 'ID',
   LoginPassword: '비밀번호',
   TFA_Code: 'Code from your TFA app',
   No_Account: '계정이 없으신가요? 회원가입하세요!',
   Forget_Password: '비번 까먹음 ㅜㅜ',
-  UserLogin: '로그인',
-  
+  UserLogin: '로그인',  
   // Register.vue
   RegisterUsername: 'ID',
   Email_Address: 'Email 주소',
@@ -99,16 +87,14 @@ export const m = {
   Password_Again: '비밀번호 재확인',
   Captcha: '로봇 확인 코드',
   UserRegister: '회원가입',
-  Already_Registed: '이미 가입되어 있나요? 로그인하세요!',
-  
+  Already_Registed: '이미 가입되어 있나요? 로그인하세요!',  
   // ResetPassword.vue and ApplyResetPassword.vue
   Reset_Password: '비번 까먹음 ㅜㅜ',
   RPassword: '비밀번호',
   RPassword_Again: '비밀번호 재확인',
   RCaptcha: '로봇 확인 코드',
   ApplyEmail: '이메일 주소',
-  Send_Password_Reset_Email: '비번 초기화 이메일 발송',
-  
+  Send_Password_Reset_Email: '비번 초기화 이메일 발송',  
   // UserHome.vue
   UserHomeSolved: '해결',
   UserHomeserSubmissions: '제출',
